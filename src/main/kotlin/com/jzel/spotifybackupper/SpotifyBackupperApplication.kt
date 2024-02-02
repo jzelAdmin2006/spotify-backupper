d1e5940db6ae0dc84af3eee399563e097c971263
Johannes Zeller <jo.zeller@protonmail.com>
2024-02-02 19:38:17 +0100

    add project

---
+++ src/main/kotlin/com/jzel/spotifybackupper/SpotifyBackupperApplication.kt
@@ -0,0 +1,11 @@
+package com.jzel.spotifybackupper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpotifyBackupperApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpotifyBackupperApplication>(*args)
+}
